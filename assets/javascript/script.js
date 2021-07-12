6b4ef59aa4a8d15343e83900867ecfbead85d040
Stephanie <stephaniehage23@gmail.com>
2021-07-11 21:29:20 -0400

    Add spontaneous selection

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+function runBuddyClick() {
+    window.location.href = "https://hages2.github.io/run-buddy/";
+};
+function projectOneClick(){
+    window.location.href = "https://danm1996.github.io/spontaneous-selection"
+}
